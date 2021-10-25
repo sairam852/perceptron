3f9065999d3cd46cc8422b86ad94b2947a3f19f4
sairam852 <racsai574@gmail.com>
2021-10-25 22:03:44 +0530

    organized manner

--- and.py
+++ and.py
@@ -3,21 +3,26 @@ from utils.all_utils import prepare_data,save_model,save_plot
 import pandas as pd
 import numpy as np
 
-AND={
-    "x1":[0,0,1,1],
-    "x2":[0,1,0,1],
-    "y":[0,0,0,1],
-}
-df=pd.DataFrame(AND)
+def main(data,eta,epochs,filename,plotFileName):
+    df=pd.DataFrame(data)
 
-print(df)
+    print(df)
+    X,y=prepare_data(df)
+    model=Perceptron(eta=eta,epochs=epochs)
+    model.fit(X,y)
+    _=model.total_loss() #dummy variable _ we can remove it whenever we want
 
-X,y=prepare_data(df)
-ETA=0.3
-EPOCHS=10
-model=Perceptron(eta=ETA,epochs=EPOCHS)
-model.fit(X,y)
-_=model.total_loss()
+    save_model(model,filename=filename)
+    save_plot(df,plotFileName,model)
 
-save_model(model,filename="and.model")
-save_plot(df,"and.png",model)
+
+if __name__== '__main__': #entry point to the whole code
+
+    AND={
+        "x1":[0,0,1,1],
+        "x2":[0,1,0,1],
+        "y":[0,0,0,1],
+    }
+    ETA=0.3
+    EPOCHS=10
+    main(data=AND,eta=ETA,epochs=EPOCHS,filename="and.model",plotFileName="and.png")
---
+++ or.py
@@ -0,0 +1,33 @@
+from utils.model import Perceptron
+from utils.all_utils import prepare_data,save_model,save_plot
+import pandas as pd
+import numpy as np
+
+from utils.model import Perceptron
+from utils.all_utils import prepare_data,save_model,save_plot
+import pandas as pd
+import numpy as np
+
+def main(data,eta,epochs,filename,plotFileName):
+    df=pd.DataFrame(data)
+
+    print(df)
+    X,y=prepare_data(df)
+    model=Perceptron(eta=eta,epochs=epochs)
+    model.fit(X,y)
+    _=model.total_loss() #dummy variable _ we can remove it whenever we want
+
+    save_model(model,filename=filename)
+    save_plot(df,plotFileName,model)
+
+
+if __name__== '__main__': #entry point to the whole code
+
+    OR={
+    "x1":[0,0,1,1],
+    "x2":[0,1,0,1],
+    "y":[0,1,1,1],
+    }
+    ETA=0.3
+    EPOCHS=10
+    main(data=OR,eta=ETA,epochs=EPOCHS,filename="or.model",plotFileName="or.png")
