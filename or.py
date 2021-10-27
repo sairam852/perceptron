ea1567bb7dab1ad62244ddbec114afe18c2f55f0
sairam852 <racsai574@gmail.com>
2021-10-27 19:02:29 +0530

    readme upadates

--- or.py
+++ or.py
@@ -1,17 +1,23 @@
+"""
+author: R sai ram reddy
+email:racsai574@gmail.com
+"""
 from utils.model import Perceptron
 from utils.all_utils import prepare_data,save_model,save_plot
 import pandas as pd
 import numpy as np
+import logging
+import os 
 
-from utils.model import Perceptron
-from utils.all_utils import prepare_data,save_model,save_plot
-import pandas as pd
-import numpy as np
+logging_str="[%(asctime)s:%(levelname)s:%(module)s] %(message)s"
+log_dir="logs/"
+os.makedirs(log_dir,exist_ok=True)
+logging.basicConfig(filename=os.path.join(log_dir,"running_logs.log"),level=logging.INFO,format=logging_str,filemode="a")
 
 def main(data,eta,epochs,filename,plotFileName):
     df=pd.DataFrame(data)
 
-    print(df)
+    logging.info(f"This  is actual dataframe{df}") 
     X,y=prepare_data(df)
     model=Perceptron(eta=eta,epochs=epochs)
     model.fit(X,y)
@@ -30,4 +36,10 @@ if __name__== '__main__': #entry point to the whole code
     }
     ETA=0.3
     EPOCHS=10
-    main(data=OR,eta=ETA,epochs=EPOCHS,filename="or.model",plotFileName="or.png")
+    try:
+        logging.info(">>>>>>>Starting training >>>>>>>")
+        main(data=OR,eta=ETA,epochs=EPOCHS,filename="or.model",plotFileName="or.png")
+        logging.info("<<<<<<<<< training done succesfully <<<<<<<<<<<\n")
+    except Exception as e:
+        logging.exception(e)
+        raise e
--- utils/all_utils.py
+++ utils/all_utils.py
@@ -4,6 +4,7 @@ import pandas as pd
 import joblib #for savig my moodel as a binary file
 from matplotlib.colors import ListedColormap
 import os
+import logging
 plt.style.use("fivethirtyeight")
 
 def prepare_data(df):
@@ -15,6 +16,7 @@ def prepare_data(df):
     Returns:
         tuple: it retuns the tuple of dependent and independent variables
     """
+    logging.info("preparing the data by segregigating the dependent variables")
     X=df.drop("y",axis=1)
     y=df["y"]
     return X,y
@@ -26,10 +28,12 @@ def save_model(model, filename):
         model (python object): trained model to 
         filename (str): path to save the trained model
     """
+    logging.info("saving the trained model")
     model_dir = "models"
     os.makedirs(model_dir, exist_ok=True) # ONLY CREATE IF MODEL_DIR DOESN"T EXISTS
     filePath = os.path.join(model_dir, filename) # model/filename
     joblib.dump(model, filePath)
+    logging.info(f"saved the trained model at{filePath}")
 
 def save_plot(df, file_name, model):
     """it is goin to save the model 
@@ -45,6 +49,7 @@ def save_plot(df, file_name, model):
         Args:
             df (python object): pandas dataframe
         """
+        logging.info("creating the base plot")
         df.plot(kind="scatter", x="x1", y="x2", c="y", s=100, cmap="winter")
         plt.axhline(y=0, color="black", linestyle="--", linewidth=1)
         plt.axvline(x=0, color="black", linestyle="--", linewidth=1)
@@ -59,6 +64,7 @@ def save_plot(df, file_name, model):
             classfier (object): model which classified
             resolution (float, optional): color. Defaults to 0.02.
         """
+        logging.info("ploting the decision regions")
         colors = ("red", "blue", "lightgreen", "gray", "cyan")
         cmap = ListedColormap(colors[: len(np.unique(y))])
 
@@ -70,8 +76,8 @@ def save_plot(df, file_name, model):
 
         xx1, xx2 = np.meshgrid(np.arange(x1_min, x1_max, resolution), 
                                np.arange(x2_min, x2_max, resolution))
-        print(xx1)
-        print(xx1.ravel())
+        #print(xx1)
+        #print(xx1.ravel())
         Z = classfier.predict(np.array([xx1.ravel(), xx2.ravel()]).T)
         Z = Z.reshape(xx1.shape)
         plt.contourf(xx1, xx2, Z, alpha=0.1, cmap=cmap)
@@ -87,3 +93,4 @@ def save_plot(df, file_name, model):
     os.makedirs(plot_dir, exist_ok=True) # ONLY CREATE IF MODEL_DIR DOESN"T EXISTS
     plotPath = os.path.join(plot_dir, file_name) # model/filename
     plt.savefig(plotPath)
+    logging.info(f"saving the plot at{plotPath}")
